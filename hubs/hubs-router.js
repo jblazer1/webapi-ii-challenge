776e67f0450a08ff898f5773279f97c42184a9b6
JamesB <jblazer1@yahoo.com>
2019-05-31 19:12:15 -0700

    I think CRUD works - not sure - thinking about quiting

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -122,5 +122,69 @@ router.get("/:id/comments", async (req, res) => {
 });
 
 // PUT ================================= PUT =============================== PUT =========================== PUT
+// router.put("/:id", async (req, res) => {
+//   if (!req.body.title || !req.body.contents) {
+//     res.status(400).json({
+//       errorMessage: "Please provide title and contents for the post."
+//     });
+//   } else {
+//     try {
+//       const hub = await Hubs.update(req.params.id, req.body);
+//       if (hub) {
+//         res.status(200).json(hub);
+//       } else {
+//         res
+//           .status(404)
+//           .json({ message: "The post witht the specified ID does not exist." });
+//       }
+//     } catch (error) {
+//       res
+//         .status(500)
+//         .json({ error: "The post information could not be motified.", error });
+//     }
+//   }
+// });
 
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.findById(id)
+    .then(userInfo => {
+      if (!userInfo) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist. " });
+      } else if (!changes.name || !changes.bio) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      } else {
+        db.update(id, changes).then(updated => {
+          res.status(200).json({ updated });
+        });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified.", error });
+    });
+});
+
+// DELETE ============================== DELETE =========================== DELETE ============================== DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await Hubs.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ success: "The delete has been successful. " });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed", error });
+  }
+});
 module.exports = router;
